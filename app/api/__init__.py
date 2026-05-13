029fc812104ccce50ae21307e8a6f66f696cbef0
Nguyen Trung Dat <datnguyentrung.ptit@gmail.com>
2026-05-13 23:17:24 +0700

    Add websocket support for virtual try-on by integrating websocket_vto_route

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,3 @@
 from app.api.virtual_try_on_route import router as virtual_try_on_router
 from app.api.chat_routes import router as chat_router
+from app.api.websocket_vto_route import router as websocket_vto_router
